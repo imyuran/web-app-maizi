260e83316879f79c9eb05c1de7f5562ae8cc72ea
yuran <836976586@qq.com>
2018-10-28 20:45:01 +0800

    return info

--- app/Http/Controllers/ExpressionController.php
+++ app/Http/Controllers/ExpressionController.php
@@ -19,10 +19,18 @@ class ExpressionController extends BaseController
         foreach ($list as &$item) {
             $key_1[] = $item->key_1;
         }
+        $key_1 = array_unique($key_1);
+
+        $new = [];
+        foreach ($key_1 as $k=>$v) {
+            $new['value'] = $v;
+            $new['text'] = $v;
+            $key_1[$k] = $new;
+        }
 
         $ret = [
             'expressions' => $list,
-            'key_1' => array_unique($key_1)
+            'key_1' => $key_1
         ];
         return $this->outPutSucc($ret);
     }
--- app/Http/Controllers/QrcodeController.php
+++ app/Http/Controllers/QrcodeController.php
@@ -4,18 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MQrcodeInfo as QrcodeInfo;
+use App\Models\MExpressionInfo as ExpressionInfo;
 
 class QrcodeController extends BaseController
 {
-    //获取二维码信息
+    //获取二维码信息,并且返回第一个表现型
     public function getQrcodeByName()
     {
         $search = request('search');
 
-        $ret = QrcodeInfo::where("name", $search)->orWhere("unique_id", $search)->first();
+        $list = QrcodeInfo::where("name", $search)->orWhere("unique_id", $search)->first();
 
-        if($ret) {
-            $ret->typeStr = config("maizi.type." . $ret->type);
+        if($list) {
+            $list->typeStr = config("maizi.type." . $list->type);
+
+            $exp = ExpressionInfo::where("steps", 21)->get();
+
+            $key_1 = [];
+            foreach ($exp as &$item) {
+                $key_1[] = $item->key_1;
+            }
+            $key_1 = array_unique($key_1);
+            $new = [];
+            foreach ($key_1 as $k=>$v) {
+                $new['value'] = $v;
+                $new['text'] = $v;
+                $key_1[$k] = $new;
+            }
+
+            $ret = [
+                'qrcode' => $list,
+                'expression' => $exp,
+                'key_1' => $key_1
+            ];
+
+
             return $this->outPutSucc($ret);
         } else {
             return $this->outPutErr("未查询到{$search}的信息！");
